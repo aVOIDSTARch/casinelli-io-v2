b6823f58dbdb3b1e12b9b79406aae45d923b8ffc
Louis Casinelli Jr <103291324+aVOIDSTARch@users.noreply.github.com>
2026-04-13 17:57:10 -0400

    Update vite.config.ts to have unique port and prevent it from changing

--- casinelli-io/vite.config.ts
+++ casinelli-io/vite.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from "vite-plus";
 export default defineConfig({
   fmt: {},
   lint: { options: { typeAware: true, typeCheck: true } },
+    server: {
+    allowedHosts: ['.casinelli.io'],
+    strictPort: true,
+    port: 3636,
+  },
 });
